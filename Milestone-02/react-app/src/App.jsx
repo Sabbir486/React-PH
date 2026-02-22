eeb92b4229ed9f6554fa673a656f68a5e7b0e66e
Sabbir486 <australiasabbir355@gmail.com>
2026-02-22 15:29:21 +0600

    Add

--- Milestone-02/react-app/src/App.jsx
+++ Milestone-02/react-app/src/App.jsx
@@ -1,13 +1,44 @@
 
 import './App.css'
 
+// Components
+
 function App() {
   return (
     <>
-      
-      <h1>Vite + React</h1>
-      
+      <h1>Vite + React</h1> 
+      <Person></Person>    
+      <Player></Player> 
+      <Sports></Sports>
     </>
+  )
+}
+
+function Person() {
+  const age = 18;
+
+  return (
+
+    <p>I am a Student {age}</p>
+  )
+}
+
+function Player(){
+  return(
+    <h1>Player Details</h1>
+  )
+}
+
+function Sports(){
+  return(
+    <div>
+      <h3>Cricket</h3>
+      <ul>
+        <li>Batting</li>
+        <li>Bowling</li>
+      </ul>
+      <h4>Loosing</h4>
+    </div>
   )
 }
 

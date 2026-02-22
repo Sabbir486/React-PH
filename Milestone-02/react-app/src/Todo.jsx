fb4eea2028ea0c20be4e540a720f343d553b8284
Sabbir486 <australiasabbir355@gmail.com>
2026-02-23 00:39:44 +0600

    Add

--- Milestone-02/react-app/src/Todo.jsx
+++ Milestone-02/react-app/src/Todo.jsx
@@ -1,5 +1,15 @@
-export default function Todo(){
-    return(
-        <li>Task: </li>
-    )
+// Export
+
+export default function Todo({task, isDone, time=0}){
+    if(isDone === false){
+        return(
+        <li>Task: {task} {time}</li>
+        )
+    }
+
+    else{
+        return(
+            <li>Do now: {task} {time}</li>
+        )
+    }
 }

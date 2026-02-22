9963bae65dd8af248f433b7eef94b1ee13896179
Sabbir486 <australiasabbir355@gmail.com>
2026-02-23 01:14:34 +0600

    Add

--- Milestone-02/react-app/src/Todo.jsx
+++ Milestone-02/react-app/src/Todo.jsx
@@ -1,15 +1,29 @@
 // Export
 
-export default function Todo({task, isDone, time=0}){
-    if(isDone === false){
-        return(
-        <li>Task: {task} {time}</li>
-        )
-    }
+// export default function Todo({task, isDone, time=0}){
+//     if(isDone === false){
+//         return(
+//         <li>Task: {task} {time}</li>
+//         )
+//     }
 
-    else{
-        return(
-            <li>Do now: {task} {time}</li>
-        )
-    }
+//     else{
+//         return(
+//             <li>Do now: {task} {time}</li>
+//         )
+//     }
+// }
+
+
+// Conditional Rendering: And (&&)
+
+// export default function Todo({task, isDone, time=0}){
+//     return isDone && <li>Done Task: {task} time: {time}</li>
+// }
+
+
+// Conditional Rendering: Or (||)
+
+export default function Todo({task, isDone, time=0}){
+    return isDone || <li>Not Done Task: {task} time: {time ? 'I am done' : 'I am not done yet'}</li>
 }

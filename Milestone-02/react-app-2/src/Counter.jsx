d997f481a1475d1e8b0158995301269f356483dd
Sabbir486 <australiasabbir355@gmail.com>
2026-02-23 16:52:06 +0600

    Add

--- Milestone-02/react-app-2/src/Counter.jsx
+++ Milestone-02/react-app-2/src/Counter.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 export default function Counter(){
 
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);  //React Hook
 
     const handleAdd = () => {
         const newCount = count + 1;

685ca3643e2f2bb6d249eeea3fdc5da3148dee0d
Sabbir486 <australiasabbir355@gmail.com>
2026-02-25 02:15:39 +0600

    Add

---
+++ Milestone-04/react-world/src/component/Country/Country.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Country = ({country}) => {
+
+    // const {common} = country;
+
+    return (
+        <div>
+            <h3>Name: {country.name.common}</h3>
+        </div>
+    );
+};
+
+export default Country;

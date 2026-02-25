aac30fa84ce09cd573517a0f580c138ea4bbacf2
Sabbir486 <australiasabbir355@gmail.com>
2026-02-25 16:56:30 +0600

    Add

--- Milestone-04/react-world/src/component/Countries/Countries.jsx
+++ Milestone-04/react-world/src/component/Countries/Countries.jsx
@@ -1,20 +1,29 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../Country/Country';
 import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
+    const [visitedCountries, setVisitedCountries] = useState([]);
+
+    const handleVisitedCountries = (country) => {
+        console.log('Country Visited clicked to be added', country);
+        
+    }
     
     const countries = use(countriesPromise);
-    console.log(countries);
+    // console.log(countries);
     
     
     return (
         <div>
             <h1>Traveling Countries: {countries.length}</h1>
+            <h3>Traveled so far: </h3>
 
             <div className='countries'>
                 {
-                countries.map(country => <Country country={country}></Country>)
+                countries.map(country => <Country country={country}
+                    handleVisitedCountries = {handleVisitedCountries}
+                ></Country>)
             }
             </div>
         </div>

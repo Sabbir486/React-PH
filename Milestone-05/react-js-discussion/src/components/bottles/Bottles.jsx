a1f21fe349c07839bfc8b76a6be83b44919ecc84
Sabbir486 <australiasabbir355@gmail.com>
2026-02-28 02:51:51 +0600

    Add

--- Milestone-05/react-js-discussion/src/components/bottles/Bottles.jsx
+++ Milestone-05/react-js-discussion/src/components/bottles/Bottles.jsx
@@ -1,14 +1,18 @@
 import React, { use } from 'react';
+import Bottle from '../bottle/Bottle';
 
 const Bottles = ({bottlesPromise}) => {
 
     const bottles = use(bottlesPromise);
-    console.log(bottles);
+    // console.log(bottles);
     
 
     return (
         <div>
-            <h2>Bottles Done</h2>
+            <h2>Bottles: {bottles.length}</h2>
+            {
+                bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+            }
         </div>
     );
 };

944bad4c3e5de64924397bfe930370829773ad71
Sabbir486 <australiasabbir355@gmail.com>
2026-02-28 04:07:59 +0600

    Add

--- Milestone-05/react-js-discussion/src/components/bottles/Bottles.jsx
+++ Milestone-05/react-js-discussion/src/components/bottles/Bottles.jsx
@@ -1,17 +1,30 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Bottle from '../bottle/Bottle';
 
 const Bottles = ({bottlesPromise}) => {
 
     const bottles = use(bottlesPromise);
     // console.log(bottles);
+
+    const [cart, setCart] = useState([]);
+
+    const handleAddtoCart = (bottle) => {
+        // console.log('Bottle will be added to cart', bottle );
+        const newCart = [...cart, bottle];
+        setCart(newCart);
+        
+    }
     
 
     return (
         <div>
             <h2>Bottles: {bottles.length}</h2>
+            <p>Added to Cart: {cart.length}</p>
             {
-                bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
+                bottles.map(bottle => <Bottle key={bottle.id}
+                     bottle={bottle}
+                     handleAddtoCart= {handleAddtoCart}
+                     ></Bottle>)
             }
         </div>
     );

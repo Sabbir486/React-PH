20798d25d6a29573056ca01fcc6c4f7625699441
Sabbir486 <australiasabbir355@gmail.com>
2026-03-07 23:32:53 +0600

    Add Navbar component and integrate it into App

--- Module-01/Milestone-06/react-project/src/Navbar.jsx
+++ Module-01/Milestone-06/react-project/src/Navbar.jsx
@@ -26,7 +26,7 @@ const navbar = () => {
             <span className="badge">New</span>
           </a>
         </li>
-        <li><a>Settings</a></li>
+        <li><a>Setting</a></li>
         <li><a>Logout</a></li>
       </ul>
     </div>

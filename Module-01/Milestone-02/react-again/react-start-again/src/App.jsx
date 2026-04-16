021d72980dd6cd5a206b77fff1e6b428dbf7c7ac
Sabbir486 <australiasabbir355@gmail.com>
2026-04-17 03:48:57 +0600

    Add

--- Module-01/Milestone-02/react-again/react-start-again/src/App.jsx
+++ Module-01/Milestone-02/react-again/react-start-again/src/App.jsx
@@ -6,8 +6,11 @@ function App() {
   return (
     <>
       <h1>React Core Concepts</h1>
-      <Person></Person>
+      {/* <Person></Person>
       <Student></Student>
+      <Developer name="Alice" language="JavaScript"></Developer>
+      <Developer name="Bob" language="Python"></Developer>
+      <Player name="Tamim" runs="10000"></Player> */}
     </>
   )
 }
@@ -20,6 +23,26 @@ function Person() {
     <p>I am {name} and I am {age} years old.</p>
   )
 }
+
+function Developer(props) {
+  return (
+    <div className="Developer">
+      <p>Name: {props.name}</p>
+      <p>Language: {props.language}</p>
+    </div>
+  )
+}
+
+function Player({name, runs}) {
+
+  return (
+    <div className="Student">
+      <p>Name: {name}</p>
+      <p>Runs: {runs}</p>
+    </div>
+  )
+}
+
 
 function Student() {
   // const name = 'Alice';

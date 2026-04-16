11d9f1084a8a0f06932a83360223568c6c11f0eb
Sabbir486 <australiasabbir355@gmail.com>
2026-04-17 03:23:22 +0600

    Add

--- Module-01/Milestone-02/react-again/react-start-again/src/App.jsx
+++ Module-01/Milestone-02/react-again/react-start-again/src/App.jsx
@@ -5,8 +5,32 @@ function App() {
 
   return (
     <>
-      <h1>Vite React</h1>
+      <h1>React Core Concepts</h1>
+      <Person></Person>
+      <Student></Student>
     </>
+  )
+}
+
+function Person() {
+  const name = 'John';
+  const age = 30;
+
+  return (
+    <p>I am {name} and I am {age} years old.</p>
+  )
+}
+
+function Student() {
+  // const name = 'Alice';
+  // const age = 20;
+  // const grade = 'A';
+
+  return (
+    <div className="Student">
+      <p>Name: </p>
+      <p>Dept: </p>
+    </div>
   )
 }
 

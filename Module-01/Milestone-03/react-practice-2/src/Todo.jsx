df7784e542fca570a1530abbf0be266a9afa7162
Sabbir486 <australiasabbir355@gmail.com>
2026-04-19 16:57:01 +0600

    Add

--- Module-01/Milestone-03/react-practice-2/src/Todo.jsx
+++ Module-01/Milestone-03/react-practice-2/src/Todo.jsx
@@ -1,10 +1,12 @@
+import './App.css';
+
 export default function Todo({todo}){
 
     const {title, userId} = todo;
 
 
     return(
-        <div>
+        <div className="todo">
             <h5>Full Title: {title}</h5>
             <p>User ID: {userId}</p>
         </div>

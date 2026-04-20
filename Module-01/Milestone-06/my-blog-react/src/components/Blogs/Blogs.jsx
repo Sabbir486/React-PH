e21931f7ee172731d28b053b6240d3ca07d18d3a
Sabbir486 <australiasabbir355@gmail.com>
2026-04-21 03:43:52 +0600

    Add

--- Module-01/Milestone-06/my-blog-react/src/components/Blogs/Blogs.jsx
+++ Module-01/Milestone-06/my-blog-react/src/components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Blog from './Blog';
 
 const Blogs = () => {
 
@@ -17,6 +18,12 @@ const Blogs = () => {
     return (
         <div>
             <h1 className='text-3xl'>Total Blogs: {blogs.length}</h1>
+
+            <div className="all-blogs grid grid-cols-2">
+                {
+                    blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                }
+            </div>
         </div>
     );
 };

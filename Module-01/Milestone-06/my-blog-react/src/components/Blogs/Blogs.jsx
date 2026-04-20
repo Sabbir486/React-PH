edfe151dff54fb153d48c150d2cd31e684502048
Sabbir486 <australiasabbir355@gmail.com>
2026-04-21 04:40:24 +0600

    Add

--- Module-01/Milestone-06/my-blog-react/src/components/Blogs/Blogs.jsx
+++ Module-01/Milestone-06/my-blog-react/src/components/Blogs/Blogs.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Blog from './Blog';
 
-const Blogs = () => {
+const Blogs = ({handleBookmark}) => {
 
     const [blogs, setBlogs] = useState([])
 
@@ -21,7 +21,7 @@ const Blogs = () => {
 
             <div className="all-blogs grid grid-cols-2">
                 {
-                    blogs.map(blog => <Blog key={blog.id} blog={blog}></Blog>)
+                    blogs.map(blog => <Blog key={blog.id} blog={blog} handleBookmark={handleBookmark}></Blog>)
                 }
             </div>
         </div>

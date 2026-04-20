82f5de70edffefbf111a0123761d36c24d74f7af
Sabbir486 <australiasabbir355@gmail.com>
2026-04-21 02:18:28 +0600

    Add

--- Module-01/Milestone-06/my-blog-react/src/App.jsx
+++ Module-01/Milestone-06/my-blog-react/src/App.jsx
@@ -1,6 +1,7 @@
-
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import Blogs from './components/Blogs/Blogs'
+
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Blogs></Blogs>
     </>
   )
 }

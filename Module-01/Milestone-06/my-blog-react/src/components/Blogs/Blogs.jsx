e757c3794aa2d5063e8ff47fa43537cd5e648200
Sabbir486 <australiasabbir355@gmail.com>
2026-04-21 02:36:12 +0600

    Add

--- Module-01/Milestone-06/my-blog-react/src/components/Blogs/Blogs.jsx
+++ Module-01/Milestone-06/my-blog-react/src/components/Blogs/Blogs.jsx
@@ -11,12 +11,12 @@ const Blogs = () => {
         )
     },[])
 
-    console.log(blogs);
+    // console.log(blogs);
     
 
     return (
         <div>
-            
+            <h1 className='text-3xl'>Total Blogs: {blogs.length}</h1>
         </div>
     );
 };

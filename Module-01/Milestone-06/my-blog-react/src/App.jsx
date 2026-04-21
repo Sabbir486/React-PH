e5aecf48e3534c7ea1bf8926f2f9380eaa1f765a
Sabbir486 <australiasabbir355@gmail.com>
2026-04-22 01:24:23 +0600

    Add

--- Module-01/Milestone-06/my-blog-react/src/App.jsx
+++ Module-01/Milestone-06/my-blog-react/src/App.jsx
@@ -7,13 +7,22 @@ import { useState } from 'react'
 function App() {
 
   const [bookmarked, setBookmarked] = useState([]);
+  const [readTime, setReadTime] = useState(0);
 
   const handleBookmark = (blog) => {
     setBookmarked([...bookmarked, blog])  
 
   }
 
-  console.log(bookmarked);
+  const handleMarkAsRead = (time) => {
+    const newTime = (readTime + time);
+    setReadTime(newTime);
+        
+  }
+
+  // console.log(bookmarked);
+  console.log(readTime);
+  
   
   
 
@@ -25,15 +34,15 @@ function App() {
       <div className="main-container flex text-center">
         <div className="left-container w-[70%]">
           
-          <Blogs handleBookmark = {handleBookmark}></Blogs>
+          <Blogs handleBookmark = {handleBookmark} handleMarkAsRead = {handleMarkAsRead}></Blogs>
         </div>
 
         <div className="right-container w-[30%]">
-          <h1>Reading time: 0</h1>
-          <h1>Bookmarked Count: 0</h1>
+          <h1>Reading time: {readTime}</h1>
+          <h1>Bookmarked Count: {bookmarked.length}</h1>
 
           {
-            bookmarked.map(marked => <p>{marked.title}</p>)
+            bookmarked.map(marked => <p className='bg-amber-300 p-2 m-3 text-white'>{marked.title}</p>)
           }
         </div>
       </div>

3955cf58cfd084329baf12cac8aa81d3d51640ed
Sabbir486 <australiasabbir355@gmail.com>
2026-04-28 23:32:10 +0600

    Add

--- Module-02/Milestone-01/new-project/src/components/Navbar/Navbar.jsx
+++ Module-02/Milestone-01/new-project/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Menu, X} from 'lucide-react';
 
 const navigationData = [
   { id: 1, name: "Home", path: "/" },
@@ -10,20 +11,41 @@ const navigationData = [
 
 
 const Navbar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const links = navigationData.map(route => <li               className='ml-7'>
+                        <a href={route.path}>{route.name}</a></li>)
+
     return (
-        <nav>
+        <nav className='flex justify-between mx-10'>
+
             {/* <ul className='flex gap-x-20'>
                 <li className='ml-7'><a href="/">Home</a></li>
                 <li><a href="/">About</a></li>
                 <li><a href="/">Blog</a></li>
             </ul> */}
 
-            <ul className='flex gap-x-20'>
+            <span className='flex' onClick={() => setOpen(!open)}>
                 {
-                    navigationData.map(route => <li className='ml-7'>
-                        <a href={route.path}>{route.name}</a></li>)
+                    open ? 
+                        <X className='md:hidden'></X> : 
+                        <Menu className='md:hidden'></Menu>
+                }
+                
+                <ul className='md:hidden'>
+                    {links}
+                </ul>
+                <h3 className='ml-4'>Navbar</h3>
+            </span>
+
+            <ul className='md:flex hidden gap-x-20'>
+                {
+                    links
                 }
             </ul>
+
+            <button>Sign In</button>
             
         </nav>
         

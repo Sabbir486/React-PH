bc355360b4e247f71f94ca4afbad337644f8455a
Sabbir486 <australiasabbir355@gmail.com>
2026-05-01 04:29:56 +0600

    Add

--- Module-02/Milestone-02/React-Router/src/components/Header/Header.jsx
+++ Module-02/Milestone-02/React-Router/src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 const Header = () => {
     return (
         <div>
             <h3>This is Header</h3>
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/mobiles'>Mobiles</Link>
+                <Link to='/laptops'>Laptops</Link>
+                <Link to='/'>Home</Link>
+            </nav>
         </div>
     );
 };

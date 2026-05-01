5c810b305208e8ce52c5079888a0d3b5faf542a5
Sabbir486 <australiasabbir355@gmail.com>
2026-05-01 22:16:10 +0600

    Add

---
+++ Module-02/Milestone-02/React-Router/src/components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p>
+                <small>Thank you for visiting our site!</small>
+                <a href="/">Visit our main page</a>
+                <a href="/contact">Contact Us</a>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
--- Module-02/Milestone-02/React-Router/src/components/Header/Header.jsx
+++ Module-02/Milestone-02/React-Router/src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 // import React from 'react';
 import { Link } from 'react-router';
+import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <h3>This is Header</h3>
+            {/* <h3>This is Header</h3> */}
             <nav>
                 <Link to='/'>Home</Link>
                 <Link to='/mobiles'>Mobiles</Link>

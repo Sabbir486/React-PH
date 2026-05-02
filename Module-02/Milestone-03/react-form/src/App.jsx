06f7a61d7822ccfb0f925d3e0c12f5457bd1580d
Sabbir486 <australiasabbir355@gmail.com>
2026-05-03 05:17:24 +0600

    Add

--- Module-02/Milestone-03/react-form/src/App.jsx
+++ Module-02/Milestone-03/react-form/src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import SimpleForm from './components/SimpleForm/SimpleForm'
 
 function App() {
   
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <h2>React Form Explore</h2>
+      <SimpleForm></SimpleForm>
     </>
   )
 }

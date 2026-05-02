d38db97b7afde18db5b92e193d0155dc1d61df8d
Sabbir486 <australiasabbir355@gmail.com>
2026-05-02 22:16:38 +0600

    Add

--- Module-02/Milestone-02/React-Router/src/components/User/User.jsx
+++ Module-02/Milestone-02/React-Router/src/components/User/User.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link, useNavigate } from 'react-router';
 
 const User = ({user}) => {
 
     const {name, email, phone} = user;
+    const navigate = useNavigate();
+
+    const handleNavigate = () => {
+        navigate(`/users/${user.id}`);
+    }
 
     const userStyle = {
         border: '1px solid yellow',
         margin: '10px',
         padding: '10px',
-        borderRadius: '5px'
+        borderRadius: '10px'
     }
 
     return (
@@ -18,6 +23,8 @@ const User = ({user}) => {
             <p>Email: {email}</p>
             <p>Phone: {phone}</p>
             <Link className='text-blue-700 border-2 p-1 rounded-2xl' to={`/users/${user.id}`}>Show Details</Link>
+
+            <button className='border-2' onClick={handleNavigate}>Details of: {user.id}</button>
         </div>
     );
 };
--- Module-02/Milestone-02/React-Router/src/components/UserDetails/UserDetails.jsx
+++ Module-02/Milestone-02/React-Router/src/components/UserDetails/UserDetails.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import { useLoaderData } from 'react-router';
+import { useLoaderData, useNavigate } from 'react-router';
 
 const UserDetails = () => {
 
     const user = useLoaderData();
     // console.log(user);
+    const navigate = useNavigate();
 
     const {website} = user;
 
@@ -13,6 +14,8 @@ const UserDetails = () => {
             <h3>User Details Here</h3>
             <h5>Name: {user.name}</h5>
             <p>Website: {website}</p>
+
+            <button className='border-2 p-2 rounded-2xl mt-3.5' onClick={() => navigate(-1)}>Go Back</button>
         </div>
     );
 };

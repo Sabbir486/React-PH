c90efe69db5f5557cfb17f5774c4e70b1341cbd5
Sabbir486 <australiasabbir355@gmail.com>
2026-05-02 16:56:19 +0600

    Add

--- Module-02/Milestone-02/React-Router/src/components/UserDetails/UserDetails.jsx
+++ Module-02/Milestone-02/React-Router/src/components/UserDetails/UserDetails.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UserDetails = () => {
+
+    const user = useLoaderData();
+    // console.log(user);
+
+    const {website} = user;
+
     return (
         <div>
             <h3>User Details Here</h3>
+            <h5>Name: {user.name}</h5>
+            <p>Website: {website}</p>
         </div>
     );
 };

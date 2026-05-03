961c7c4d305161c49deacc1f520fb08e045597a0
Sabbir486 <australiasabbir355@gmail.com>
2026-05-04 00:35:25 +0600

    Add

---
+++ Module-02/Milestone-03/react-form/src/components/UnControlledField/UnControlledField.jsx
@@ -0,0 +1,31 @@
+import React, { use } from 'react';
+import { useRef } from 'react';
+
+const UnControlledField = () => {
+
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(emailRef.current.value);
+        console.log(passwordRef.current.value);
+    }
+
+
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input ref={passwordRef} type="password" name='password' placeholder='Password' />
+                <br />
+                <input ref={emailRef} type="email" name='email' placeholder='Email' />
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+            
+        </div>
+    );
+};
+
+export default UnControlledField;

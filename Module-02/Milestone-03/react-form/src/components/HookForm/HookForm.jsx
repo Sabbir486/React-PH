ac27212d638feb291eda1c90a4ebd831bbca2e9d
Sabbir486 <australiasabbir355@gmail.com>
2026-05-04 22:21:45 +0600

    Add

---
+++ Module-02/Milestone-03/react-form/src/components/HookForm/HookForm.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import useInputField from '../../hooks/useInputField';
+
+const HookForm = () => {
+
+    const [name, handleNameChange] = useInputField('');
+    const [email, handleEmailChange] = useInputField('');
+    const [password, handlePasswordChange] = useInputField('');
+    
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('Submit', name, email, password);
+        
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Name' value={name} onChange={handleNameChange} defaultValue={name}/>
+
+                <br />
+                <input type="email" placeholder='Email' value={email} onChange={handleEmailChange} defaultValue={email}/>
+                <br />
+                <input type="password" placeholder='Password' value={password} onChange={handlePasswordChange} defaultValue={password}/>
+                <br />
+                <input type="submit" value="Submit" />
+            </form>
+        </div>
+    );
+};
+
+export default HookForm;

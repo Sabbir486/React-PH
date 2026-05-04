b38317e91b749345a0f0c47cf06149b42dfc64f2
Sabbir486 <australiasabbir355@gmail.com>
2026-05-04 22:22:03 +0600

    Add

---
+++ Module-02/Milestone-03/react-form/src/hooks/useInputField.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+
+const useInputField = (defaultValue) => {
+    
+    const [fieldValue, setFieldValue] = useState(defaultValue);
+
+    const handleFieldChange = (event) => {
+        setFieldValue(event.target.value);
+    }
+
+    return [fieldValue, handleFieldChange];
+};
+
+export default useInputField;

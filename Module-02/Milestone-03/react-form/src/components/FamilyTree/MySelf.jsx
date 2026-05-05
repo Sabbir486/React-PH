50961e8fd37aef336ec85f73d79d83a64b8dbf85
Sabbir486 <australiasabbir355@gmail.com>
2026-05-06 03:01:28 +0600

    Add

--- Module-02/Milestone-03/react-form/src/components/FamilyTree/MySelf.jsx
+++ Module-02/Milestone-03/react-form/src/components/FamilyTree/MySelf.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Specisl from './Specisl';
 
 const MySelf = () => {
     return (
         <div>
             <h3>MySelf</h3>
+            <Specisl></Specisl>
         </div>
     );
 };

832e2c66db69d9e07910463414783312f910c474
Sabbir486 <australiasabbir355@gmail.com>
2026-05-06 03:10:44 +0600

    Add

--- Module-02/Milestone-03/react-form/src/components/FamilyTree/MySelf.jsx
+++ Module-02/Milestone-03/react-form/src/components/FamilyTree/MySelf.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Specisl from './Specisl';
 
-const MySelf = () => {
+const MySelf = ({ asset }) => {
     return (
         <div>
             <h3>MySelf</h3>
-            <Specisl></Specisl>
+            <Specisl asset={asset}></Specisl>
         </div>
     );
 };

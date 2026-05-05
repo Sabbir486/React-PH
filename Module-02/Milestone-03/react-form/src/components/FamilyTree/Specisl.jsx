3b399ab2a38a2fa82317edee13b4033ace4bdf94
Sabbir486 <australiasabbir355@gmail.com>
2026-05-06 03:01:40 +0600

    Add

---
+++ Module-02/Milestone-03/react-form/src/components/FamilyTree/Specisl.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Specisl = ({ name }) => {
+    return (
+        <div>
+            <h3>My Special One</h3>
+            <p>{name}</p>
+        </div>
+    );
+};
+
+export default Specisl;

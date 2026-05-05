3e7fe9f63e56730e7bfe97787be21262aad950dd
Sabbir486 <australiasabbir355@gmail.com>
2026-05-06 02:56:03 +0600

    Add

--- Module-02/Milestone-03/react-form/src/components/FamilyTree/Aunt.jsx
+++ Module-02/Milestone-03/react-form/src/components/FamilyTree/Aunt.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Cousin from './Cousin';
 
 const Aunt = () => {
     return (
         <div>
             <h3>Aunt</h3>
+            <section className='flex'>
+                <Cousin name='Abida'></Cousin>
+                <Cousin name='Albina'></Cousin>
+                <Cousin name='Jani na'></Cousin>
+            </section>
         </div>
     );
 };

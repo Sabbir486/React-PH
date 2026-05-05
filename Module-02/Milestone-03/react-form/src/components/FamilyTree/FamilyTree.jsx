9c3cccf96bec4d333f5729f23ac16f11c9f58d0e
Sabbir486 <australiasabbir355@gmail.com>
2026-05-06 03:25:13 +0600

    Add

--- Module-02/Milestone-03/react-form/src/components/FamilyTree/FamilyTree.jsx
+++ Module-02/Milestone-03/react-form/src/components/FamilyTree/FamilyTree.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import Grandpa from './Grandpa';
 import './FamilyTree.css';
+import { createContext } from 'react';
+
+export const AssetContext = createContext('');
 
 const FamilyTree = () => {
 
     const asset = 'diamond';
+    const asset2 = 'gold';
 
     return (
         <div className='family-tree'>
             <h2>Family Tree</h2>
-            <Grandpa asset={asset}></Grandpa>
+            <AssetContext.Provider value={asset2}>
+                <Grandpa asset={asset}></Grandpa>
+            </AssetContext.Provider>
         </div>
     );
 };
--- Module-02/Milestone-03/react-form/src/components/FamilyTree/Specisl.jsx
+++ Module-02/Milestone-03/react-form/src/components/FamilyTree/Specisl.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AssetContext } from './FamilyTree';
 
 const Specisl = ({ name, asset }) => {
+
+    const Asset2 = useContext(AssetContext);
+    // console.log("New Asset: ", Asset2);
+    
+
     return (
         <div>
             <h3>My Special One</h3>
             <p>{name}</p>
             <p>Asset: {asset}</p>
+            <p>New Asset: {Asset2}</p>
         </div>
     );
 };
